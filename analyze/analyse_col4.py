7b7775392d19c69e31009077dee42a463b7d6301
dyeden <dyedenm@gmail.com>
2019-08-08 10:35:43 -0300

    grafico crescimento  percentual

--- analyze/analyse_col4.py
+++ analyze/analyse_col4.py
@@ -16,18 +16,16 @@ def get_area(asset_imc, geometry, year):
 
 def get_stats_cobertura_mun(municipio_name, col=4):
     if col == 4:
+        years = range(1985, 2019)
         asset_imc = 'projects/mapbiomas-workspace/TRANSVERSAIS/INFRAURBANA4-FT'
     elif col == 3:
+        years = range(1985, 2018)
         asset_imc = 'projects/mapbiomas-workspace/TRANSVERSAIS/INFRAURBANA3-FT-FINAL'
     
     municipios = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/municipios-2016');
     municipio = ee.Feature(municipios.filterMetadata('NM_MUNICIP', 'equals', municipio_name).first())
     geometry = municipio.geometry()
     area_mun = geometry.area().divide(10000)
-    
-    years = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 
-        1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 
-        2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
     
     info = []
     
